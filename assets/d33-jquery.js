8eb5ebbe0947ba2fb52f92c7355d45d70ff339bd
Pedro Caballero <pedrocaballero@iMac-de-Pedro.local>
2022-10-25 12:32:25 +0200

    jquery

--- assets/d33-jquery.js
+++ assets/d33-jquery.js
@@ -1,6 +1,6 @@
 
 (function ($) {
     $( document ).ready(function() {
-        $( ".d33-featured-collection .sf-slider__controls" ).appendTo( $( ".d33-featured-collection .section__header" ) );
+        //$( ".d33-featured-collection .sf-slider__controls" ).appendTo( $( ".d33-featured-collection .section__header" ) );
     });
 })(jQuery);
